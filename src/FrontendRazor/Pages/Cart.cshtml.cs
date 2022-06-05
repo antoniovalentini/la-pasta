cffd592b2b8e09e39c89a0a45dedabc18d7836cb
antoniovalentini <antonio.valentini@live.it>
2022-06-05 11:32:47 +0200

    rename privacy page

---
+++ src/FrontendRazor/Pages/Cart.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LaPasta.FrontendRazor.Pages;
+
+public class CartModel : PageModel
+{
+    private readonly ILogger<CartModel> _logger;
+
+    public CartModel(ILogger<CartModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
